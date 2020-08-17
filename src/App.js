d411ab30612d59d178a98d3a4f381f2efb9b2d12
amkorousagi <39821875+amkorousagi@users.noreply.github.com>
2020-08-17 14:43:19 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,24 +9,6 @@ import routes from 'src/routes';
 import { Route, BrowserRouter } from 'react-router-dom';
 
 
-import { Navigate } from 'react-router-dom';
-import DashboardLayout from 'src/layouts/DashboardLayout';
-import MainLayout from 'src/layouts/MainLayout';
-import AccountView from 'src/views/account/AccountView';
-import CustomerListView from 'src/views/customer/CustomerListView';
-import DashboardView from 'src/views/reports/DashboardView';
-import LoginView from 'src/views/auth/LoginView';
-import NotFoundView from 'src/views/errors/NotFoundView';
-import ProductListView from 'src/views/product/ProductListView';
-import RegisterView from 'src/views/auth/RegisterView';
-import SettingsView from 'src/views/settings/SettingsView';
-
-import Trendup_Home from 'src/views/Trendup'
-import Trendup_Clothes from 'src/views/Trendup/clothes'
-import Trendup_Clothes_Map from 'src/views/Trendup/clothes/trendmap'
-import Trendup_Clothes_Rank from 'src/views/Trendup/clothes/trendrank'
-import Trendup_Clothes_Future from 'src/views/Trendup/clothes/trendfuture'
-import Trendup_Keyword from 'src/views/Trendup/keyword'
 
 
 const App = () => {
